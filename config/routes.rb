f030fc675ac6f8f24ba72f8a6564ada9a2271f96
naoki yamada <naki@LAPTOP-I9DD1CEG.localdomain>
2022-10-11 09:45:39 +0900

    add admin

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   devise_for :users
   resources :users, only: [:show] do
     collection do
+      get :admin_settings
       get :settings
     end
     member do
--- db/schema.rb
+++ db/schema.rb
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(version: 2022_10_10_063856) do
     t.string "name", default: "", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.boolean "admin", default: false, null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
