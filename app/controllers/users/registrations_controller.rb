c7128d1d0339c0b76f839155d36f6fb1641cde56
naoki yamada <naki@LAPTOP-I9DD1CEG.localdomain>
2022-10-23 19:36:02 +0900

    ログイン周りの改良2

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,6 +1,9 @@
 class Users::RegistrationsController < Devise::RegistrationsController
   protected
-  def update_resource(resource, params)
-    resource.update_without_password(params)
+  def after_sign_up_path_for(resource)
+    user_path(resource)
+  end
+  def after_update_path_for(resource)
+    user_path(resource)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -34,7 +34,10 @@ Rails.application.routes.draw do
     end
   end
 
-  devise_for :users
+  devise_for :users, controllers: {
+    sessions: "users/sessions",
+    registrations: "users/registrations"
+  }
 
   resources :users, only: [:show] do
     collection do
