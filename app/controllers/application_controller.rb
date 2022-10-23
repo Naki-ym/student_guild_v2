c47ee84d0cab524dcdb06ffd4f5930db0a5b54cb
naoki yamada <naki@LAPTOP-I9DD1CEG.localdomain>
2022-10-23 13:14:15 +0900

    ログイン周りの改良

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,8 @@ class ApplicationController < ActionController::Base
   end
 
   private
-  def configure_permitted_parameters  
-    devise_parameter_sanitizer.permit(:sign_up,keys:[:email])
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up,keys:[:name, :email])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :email])
   end  
 end
